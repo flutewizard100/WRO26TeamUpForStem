0b9afd3a11fca5c3a2ab8fc72fa8b57837df1952
s4gn1k712 <sagnikbiswas712@gmail.com>
2026-07-10 15:28:23 -0400

    Updated Motor.py for current motor controller

--- Motor.py
+++ Motor.py
@@ -2,30 +2,34 @@ import serial
 import time
 from rclpy.node import Node
 from std_msgs.msg import Int32 
+import rclpy
 
 class Motor(Node):
 
     def __init__(self):
-
         super().__init__("Motor")
         self.motor = self.create_publisher(Int32, 'Motor', 10)
         self.subscription = self.create_subscription(Int32, 'motor_speed', self.motor_callback, 10)
-        self.direction = True
 
     def motor_callback(self, msg):
         self.setSpeed(msg.data)
 
     def setSpeed(self, speed):
+
+
+        final_us = max(1000, min(2000, int(speed)))
+
         motor_msg = Int32()
-
-        if not self.direction:
-           speed = -speed
-
-        motor_msg.data = int(max(-255, min(255, speed)))
+        motor_msg.data = final_us
         self.motor.publish(motor_msg)
 
-    def setDirection(self, direction):
-        if direction == "REVERSE":
-            self.direction = False
-        else:
-            self.direction = True
+
+    def setTime(self, target_speed, duration_seconds):
+       
+                
+        self.setSpeed(target_speed)
+            
+        rclpy.spin_once(self, timeout_sec=0.01)
+        time.sleep(duration_seconds)
+        self.setSpeed(1500)
+        rclpy.spin_once(self, timeout_sec=0.01)
