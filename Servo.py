e8255f9a293fceb645bd27a7ac1b6abbde626110
s4gn1k712 <sagnikbiswas712@gmail.com>
2026-07-17 11:17:01 -0400

    Update Servo.py
    Changed program to receive data from topic command velocity and publish it to topic servo_angle

--- Servo.py
+++ Servo.py
@@ -1,29 +1,26 @@
-
+#!/usr/bin/env python3
 import serial
 import time
 from rclpy.node import Node
 from std_msgs.msg import Int32 
+from geometry_msgs.msg import Twist
 
 class Servo(Node):
     def __init__(self):
 
         super().__init__('Servo')
-        self.servo = self.create_publisher(Int32, 'Servo', 10)
-        self.subscription = self.create_subscription(
-            Int32,
-            'servo_angle',
-            self.servo_callback,
-            10
-        )
- 
-
-    def servo_callback(self, msg):
-        self.write(msg.data)
+        self.servo = self.create_publisher(Int32, 'servo_angle', 10)
+        self.cmd = self.create_subscription(Twist, 'cmd_vel', self.sendCommand, 10)
+        
 
     def write(self, angle):
         servo_msg = Int32()
         servo_msg.data = int(max(0, min(360, angle)))
         self.servo.publish(servo_msg)
-
+    
+    def sendCommand(self, msg):
+        servo_msg = Int32()
+        servo_msg.data = int(max(0, min(360, msg.angular.z)))
+        self.servo.publish(servo_msg)
 
 
