7f9ca66e2631caf1cfc6aa05adbdfb85e8192e23
s4gn1k712 <sagnikbiswas712@gmail.com>
2026-07-24 15:51:34 -0400

    Update Motor.py
    Motor has been updated so that it scales the speed before sending it to teensy.

--- Motor.py
+++ Motor.py
@@ -2,13 +2,33 @@ import serial
 import time
 from rclpy.node import Node
 from std_msgs.msg import Int32 
+from geometry_msgs.msg import Twist
 import rclpy
 
 class Motor(Node):
 
     def __init__(self):
         super().__init__("Motor")
+
         self.motor = self.create_publisher(Int32, 'motor_speed', 10)
+        self.cmd = self.create_subscription(Twist, 'cmd_vel', self.sendCommand, 10)
+
+
+    def sendCommand(self, msg):
+
+        motor_msg = Int32()
+
+        direction = msg.linear.x
+
+        if direction >= 0.0:
+            motor_msg.data = int(1500 + direction * 180)
+        else:
+            motor_msg.data = int(1500 + direction * 280)
+
+        # Clamp to valid ESC range
+        motor_msg.data = max(1000, min(2000, motor_msg.data))
+
+        self.motor.publish(motor_msg)
 
 
     def setSpeed(self, speed):
@@ -30,3 +50,5 @@ class Motor(Node):
         time.sleep(duration_seconds)
         self.setSpeed(1500)
         rclpy.spin_once(self, timeout_sec=0.01)
+
+
