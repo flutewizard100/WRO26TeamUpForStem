1a966434b31569417b8048be8987cfe0e253b144
s4gn1k712 <sagnikbiswas712@gmail.com>
2026-08-12 19:25:51 -0400

    Update RobotController.py
    Changed RobotController.py so it incoporates all nodes made so far for servo and motor.

--- RobotController.py
+++ RobotController.py
@@ -1,12 +1,33 @@
+import rclpy
 import serial
 import time
 from rclpy.node import Node
 from std_msgs.msg import Int32 
 from geometry_msgs.msg import Twist
+from WRORobot.Motor import Motor
+from WRORobot.Servo import Servo
+
 class controller(Node):
     def __init__(self):
         super().__init__("controller")
         self.send = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.motor = Motor()
+        self.servo = Servo()
 
     def sendMovement(self, msg):
         self.send.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = controller()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
