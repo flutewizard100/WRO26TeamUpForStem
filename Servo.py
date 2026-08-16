8e03c4ab912a6a5d97afb689544ff7bd1dc931bd
s4gn1k712 <sagnikbiswas712@gmail.com>
2026-08-16 17:02:17 -0400

    Update Servo.py
    Added main so it's possible to add a node to the launch file and clamped it to the range of 40 to 150 degrees

--- Servo.py
+++ Servo.py
@@ -4,7 +4,7 @@ import time
 from rclpy.node import Node
 from std_msgs.msg import Int32 
 from geometry_msgs.msg import Twist
-
+import rclpy
 class Servo(Node):
     def __init__(self):
 
@@ -15,12 +15,26 @@ class Servo(Node):
 
     def write(self, angle):
         servo_msg = Int32()
-        servo_msg.data = int(max(0, min(360, angle)))
+        servo_msg.data = int(max(40, min(150, angle)))
         self.servo.publish(servo_msg)
     
     def sendCommand(self, msg):
         servo_msg = Int32()
-        servo_msg.data = int(max(0, min(360, msg.angular.z)))
+        servo_msg.data = int(max(-10, min(10, msg.angular.z * 10)))
         self.servo.publish(servo_msg)
 
+def main(args=None):
+    rclpy.init(args=args)
 
+    node = Servo()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+     main()
